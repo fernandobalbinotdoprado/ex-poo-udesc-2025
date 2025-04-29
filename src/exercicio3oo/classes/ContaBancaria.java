c40caefb189561d3095905651b99eaaa6dceca6e
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-04-29 20:39:47 -0300

    ContaBancaria e respectivo teste exercicio3

---
+++ src/exercicio3oo/classes/ContaBancaria.java
@@ -0,0 +1,30 @@
+package exercicio3oo.classes;
+
+public class ContaBancaria {
+    private String numeroConta;
+    private String titular;
+    private double saldo;
+
+    public String getNumeroConta() {
+        return this.numeroConta;
+    }
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ src/exercicio3oo/testes/TestaContaBancaria.java
@@ -0,0 +1,14 @@
+package exercicio3oo.testes;
+import exercicio2oo.classes.ContaBancaria;
+
+class TestaContaBancaria {
+    public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria();
+        conta.setNumeroConta("Cc4587");
+        conta.setTitular("Guilherme");
+        conta.setSaldo(2345);
+        System.out.println("Numero da conta :" + conta.getNumeroConta());
+        System.out.println("Titular :" + conta.getTitular());
+        System.out.println("Saldo da conta :" + conta.getSaldo());
+    }
+}
