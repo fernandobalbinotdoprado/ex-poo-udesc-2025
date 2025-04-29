ebec1bf66755af6af21aa99bed5b27b56704b593
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-04-29 20:36:04 -0300

    Classe Aluno e respectivo teste exercicio3

---
+++ src/exercicio3oo/testes/TestaAluno.java
@@ -0,0 +1,37 @@
+package exercicio3oo.testes;
+
+
+import exercicio2oo.classes.Aluno;
+
+class TestaAluno {
+     public static void main(String[] args){
+
+        Aluno fernando = new Aluno(); // .
+        fernando.setNome("Fernando Balbinot");
+        fernando.setIdade(18);
+        fernando.setNota1(7);
+        fernando.setNota2(8);
+        fernando.setNota3(9);
+        fernando.setNota4(10);
+        System.out.println("Nome: " + fernando.getNome());
+        System.out.println("Idade: " + fernando.getIdade());
+        System.out.println("Nota 1: " + fernando.getNota1());
+        System.out.println("Nota 2: " + fernando.getNota2());
+        System.out.println("Nota 3: " + fernando.getNota3());
+        System.out.println("Nota 4: " + fernando.getNota4());
+
+        Aluno guilherme = new Aluno();
+        guilherme.setNome("Guilherme Pierdoná");
+        guilherme.setIdade(19);
+        guilherme.setNota1(7);
+        guilherme.setNota2(8);
+        guilherme.setNota3(9);
+        guilherme.setNota4(10);
+        System.out.println("Nome: " + guilherme.getNome());
+        System.out.println("Idade: " + guilherme.getIdade());
+        System.out.println("Nota 1: " + guilherme.getNota1());
+        System.out.println("Nota 2: " + guilherme.getNota2());
+        System.out.println("Nota 3: " + guilherme.getNota3());
+        System.out.println("Nota 4: " + guilherme.getNota4());
+     }
+}
