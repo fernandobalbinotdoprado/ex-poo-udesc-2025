10635aa45dcd1957ecba3bc370ef0c278d52d592
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-04-29 20:37:44 -0300

    Classe Carro e respectivo teste exercicio3

---
+++ src/exercicio3oo/testes/TestaCarro.java
@@ -0,0 +1,17 @@
+package exercicio3oo.testes;
+import exercicio2oo.classes.Carro;
+// .
+class TestaCarro {
+    public static void main(String[] args) {
+
+        Carro carro = new Carro();
+        carro.setModelo("Uno");
+        carro.setMarca("Fiat");
+        carro.setAno(2010);
+        carro.setVelocidade(200);
+        System.out.println("Modelo: " + carro.getModelo());
+        System.out.println("Marca: " + carro.getMarca());
+        System.out.println("Ano: " + carro.getAno());
+        System.out.println("Velocidade: " + carro.getVelocidade());
+    }
+}
