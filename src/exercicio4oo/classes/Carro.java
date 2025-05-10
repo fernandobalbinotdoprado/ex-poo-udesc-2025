ced591f217d77caa1269480644d7966c4e920431
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-05-09 22:28:20 -0300

    Classe Carro e respectivo teste ex4

---
+++ src/exercicio4oo/classes/Carro.java
@@ -0,0 +1,40 @@
+package exercicio4oo.classes;
+// .
+public class Carro {
+  private String modelo;
+  private String marca;
+  private int ano;
+  private double velocidade;
+
+  public String getModelo() {
+    return this.modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+  public String getMarca() {
+    return this.marca;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public int getAno() {
+    return this.ano;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
+  }
+
+  public double getVelocidade() {
+    return this.velocidade;
+  }
+
+  public void setVelocidade(double velocidade) {
+    this.velocidade = velocidade;
+  }
+}
---
+++ src/exercicio4oo/testes/TestaCarro.java
@@ -0,0 +1,41 @@
+package exercicio4oo.testes;
+import exercicio4oo.classes.Carro;
+// .
+class TestaCarro {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.setModelo("Uno");
+        carro.setMarca("Fiat");
+        carro.setAno(2010);
+        carro.setVelocidade(200);
+        System.out.println("Modelo: " + carro.getModelo());
+        System.out.println("Marca: " + carro.getMarca());
+        System.out.println("Ano: " + carro.getAno());
+        System.out.println("Velocidade: " + carro.getVelocidade());
+
+        System.out.println("\nVerificando velocidade...");
+        veloAtual(carro);
+
+        System.out.println("\nAcelerando...");
+        aumentarVelo(carro);
+
+        System.out.println("\nVerificando velocidade...");
+        veloAtual(carro);
+
+        System.out.println("\nFreiando...");
+        diminuirVelo(carro);
+
+        System.out.println("\nVerificando velocidade...");
+        veloAtual(carro);
+    }
+    static void aumentarVelo(Carro carro) {
+        carro.setVelocidade(carro.getVelocidade() + 10);
+    }
+
+    static void diminuirVelo(Carro carro) {
+        carro.setVelocidade(carro.getVelocidade() - 10);
+    }
+    static void veloAtual(Carro carro) {
+        System.out.println("Velocidade atual: " + carro.getVelocidade() + " km/h");
+    }
+}
