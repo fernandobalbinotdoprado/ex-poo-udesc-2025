307e37c376d181dc900423da2caa0f5de83f0893
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-06-01 12:05:20 -0300

    Classe ContaBancaria e respectivo teste

---
+++ src/exercicio6oo/classes/ContaBancaria.java
@@ -0,0 +1,54 @@
+package exercicio6oo.classes;
+
+public class ContaBancaria {
+    private String numeroConta;
+    private String titular;
+    private double saldo;
+
+    public ContaBancaria(){
+
+    }
+    public ContaBancaria(String numeroConta, String titular) {
+        this.numeroConta = numeroConta;
+        this.titular = titular;
+    }
+    public ContaBancaria(String numeroConta, String titular, double saldo) {
+        this(numeroConta, titular);
+        this.saldo = saldo;
+    }
+    @Override
+    public String toString() {
+        return String.format("\nNumero da conta: %s \nTitular: %s \nSaldo R$: %.1f \n",
+                this.numeroConta, this.titular, this.saldo);
+    }
+
+    public String getNumeroConta() {
+        return this.numeroConta;
+    }
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        if (saldo < 0) {
+            System.out.println("Saldo negativo");
+            return;
+        } else {
+            System.out.println("Saldo positivo");
+        }
+        this.saldo = saldo;
+    }
+
+}
