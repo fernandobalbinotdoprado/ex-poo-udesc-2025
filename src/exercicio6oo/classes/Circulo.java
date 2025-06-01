8da5f94003a1098fb0670d42566adbc3130b80dd
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-06-01 12:04:42 -0300

    Classe Circulo e respectivo teste

---
+++ src/exercicio6oo/classes/Circulo.java
@@ -0,0 +1,32 @@
+package exercicio6oo.classes;
+
+public class Circulo {
+    private double raio;
+
+    public Circulo(){
+
+    }
+    public Circulo(double raio) {
+        this();
+        this.raio = raio;
+    }
+    @Override
+    public String toString() {
+        return String.format("Raio: %.1f", this.raio);
+    }
+
+    public double getRaio() {
+        if (raio >= 0 && raio <= 1000) {
+            System.out.println("Raio válido");
+        } else {
+            System.out.println("Raio inválido");
+            return 0;
+        }
+        return this.raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+        }
+        }
+
