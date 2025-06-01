e86e76fa833650a82ef8531fabb751d48f675f46
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-06-01 12:03:27 -0300

    Classe Aluno e respectivo teste

---
+++ src/exercicio6oo/classes/Aluno.java
@@ -0,0 +1,97 @@
+package exercicio6oo.classes;
+public class Aluno {
+    private String nome;
+    private int idade;
+    private int nota1, nota2, nota3, nota4;
+
+    public Aluno() {
+
+    }
+    public Aluno(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public Aluno(String nome, int idade, int nota1, int nota2, int nota3, int nota4) {
+        this(nome, idade);
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.nota4 = nota4;
+    }
+        @Override
+    public String toString () {
+        return String.format("\nNome: %s \nidade: %d \nNota1: %d \nNota2: %d \nNota3: %d \nNota4: %d ",
+                this.nome, this.idade, this.nota1, this.nota2, this.nota3, this.nota4);
+    }
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public int getNota1() {
+        return this.nota1;
+    }
+
+    public int getNota2() {
+        return this.nota2;
+    }
+
+    public int getNota3() {
+        return this.nota3;
+    }
+
+    public int getNota4() {
+        return this.nota4;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNota1(int nota1) {
+        if (nota1 >= 0 && nota1 <= 10) {
+            System.out.println("Nota1 válida");
+        } else {
+            System.out.println("Nota inválida");
+            return;
+        }
+        this.nota1 = nota1;
+    }
+
+    public void setNota2(int nota2) {
+        if (nota2 >= 0 && nota2 <= 10) {
+            System.out.println("Nota2 válida");
+        } else {
+            System.out.println("Nota2 inválida");
+            return;
+        }
+        this.nota2 = nota2;
+    }
+
+    public void setNota3(int nota3) {
+        if (nota3 >= 0 && nota3 <= 10) {
+            System.out.println("Nota3 válida");
+        } else {
+            System.out.println("Nota3 inválida");
+            return;
+        }
+        this.nota3 = nota3;
+    }
+
+    public void setNota4(int nota4) {
+        if (nota4 >= 0 && nota4 <= 10) {
+            System.out.println("Nota4 válida");
+        } else {
+            System.out.println("Nota4 inválida");
+            return;
+        }
+        this.nota4 = nota4;
+    }
+}
---
+++ src/exercicio6oo/testes/TestaAluno.java
@@ -0,0 +1,50 @@
+package exercicio6oo.testes;
+import exercicio5oo.classes.Aluno;
+
+class TestaAluno {
+     public static void main(String[] args){
+        calcularMedia(7,8,9,10);
+        Aluno fernando = new Aluno(); // .
+        fernando.setNome("Fernando Balbinot");
+        fernando.setIdade(18);
+        fernando.setNota1(7);
+        fernando.setNota2(8);
+        fernando.setNota3(9);
+        fernando.setNota4(10);
+        System.out.println("Nome: " + fernando.getNome());
+        System.out.println("Idade: " + fernando.getIdade());
+        System.out.println("Nota 1: " + fernando.getNota1());
+        System.out.println("Nota 2: " + fernando.getNota2());
+        System.out.println("Nota 3: " + fernando.getNota3());
+        System.out.println("Nota 4: " + fernando.getNota4());
+
+        Aluno guilherme = new Aluno();
+        guilherme.setNome("Guilherme Pierdoná");
+        guilherme.setIdade(19);
+        guilherme.setNota1(7);
+        guilherme.setNota2(8);
+        guilherme.setNota3(9);
+        guilherme.setNota4(10);
+        System.out.println("Nome: " + guilherme.getNome());
+        System.out.println("Idade: " + guilherme.getIdade());
+        System.out.println("Nota 1: " + guilherme.getNota1());
+        System.out.println("Nota 2: " + guilherme.getNota2());
+        System.out.println("Nota 3: " + guilherme.getNota3());
+        System.out.println("Nota 4: " + guilherme.getNota4());
+
+        Aluno aluno1 = new Aluno("Fernando Balbinot",18, 7, 8, 9, 10);
+        System.out.println(aluno1);
+
+        Aluno aluno2 = new Aluno("Guilherme Pierdoná", 19, 7, 8, 9, 10);
+        System.out.println(aluno2);
+        }
+   static void calcularMedia(int nota1,int nota2,int nota3,int nota4) {
+      int media = (nota1 + nota2 + nota3 + nota4)/4;
+      System.out.println("Média: " + media);
+      if (media >= 7) {
+         System.out.println("Aprovado");
+      } else {
+         System.out.println("Reprovado");
+      }
+   }
+   }
