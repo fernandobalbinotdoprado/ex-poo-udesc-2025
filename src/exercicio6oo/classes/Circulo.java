06cce211c29e9b8dfbc1504266f35dad16333fe9
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-06-20 11:18:45 -0300

    Correção exercicio6oo classe Circulo e teste

--- src/exercicio6oo/classes/Circulo.java
+++ src/exercicio6oo/classes/Circulo.java
@@ -28,5 +28,21 @@ public class Circulo {
     public void setRaio(double raio) {
         this.raio = raio;
         }
-        }
+
+    public void calcularDiametro() {
+        double diametro;
+        diametro = 2 * raio;
+        System.out.println("Diametro: " + diametro);
+    }
+    public void calcularPerimetro() {
+        double perimetro;
+        perimetro = 2 * Math.PI * raio;
+        System.out.println("Perimetro: " + String.format("%.2f",perimetro));
+    }
+    public void calcularArea() {
+        double area;
+        area = (Math.PI * raio*raio);
+        System.out.println("Area: " + String.format("%.2f",area));
+    }
+   }
 
--- src/exercicio6oo/testes/TestaCirculo.java
+++ src/exercicio6oo/testes/TestaCirculo.java
@@ -1,31 +1,16 @@
 package exercicio6oo.testes;
-import exercicio5oo.classes.Circulo;
+import exercicio6oo.classes.Circulo;
 
 class TestaCirculo {
     public static void main(String[] args) {
-        calcularDiametro(5);
-        calcularPerimetro(5);
-        calcularArea(5);
         Circulo circulo = new Circulo();
         circulo.setRaio(5);
         System.out.println("Raio do Círculo: " + circulo.getRaio());
+        circulo.calcularDiametro();
+        circulo.calcularPerimetro();
+        circulo.calcularArea();
 
         Circulo circulo1 = new Circulo(5);
         System.out.println(circulo1);
-    }
-    static void calcularDiametro(double raio) {
-        double diametro;
-        diametro = 2 * raio;
-        System.out.println("Diametro: " + diametro);
-    }
-    static void calcularPerimetro(double raio) {
-        double perimetro;
-        perimetro = 2 * Math.PI * raio;
-        System.out.println("Perimetro: " + String.format("%.2f",perimetro));
-    }
-    static void calcularArea(double raio) {
-        double area;
-        area = (Math.PI * raio*raio);
-        System.out.println("Area: " + String.format("%.2f",area));
     }
 }
