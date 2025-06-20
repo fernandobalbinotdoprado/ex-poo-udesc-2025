9b0b8dc44cf10b21baccbc0b3cb9bd33b7c7df82
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-06-20 11:19:56 -0300

    Correção exercicio6oo classe Livro e teste

--- src/exercicio6oo/classes/Livro.java
+++ src/exercicio6oo/classes/Livro.java
@@ -62,5 +62,31 @@ public class Livro {
   }
   this.emprestado = emprestado;
  }
+ public void verificaEmprestimo(Livro livro) {
+  if (livro.getEmprestado()) {
+   System.out.println("O livro está emprestado.");
+  } else {
+   System.out.println("O livro está disponível.");
+  }
+ }
+
+ public void emprestaLivro(Livro livro) {
+  System.out.println("Estado antes de emprestar: " + livro.getEmprestado());
+  if (!livro.getEmprestado()) {
+   livro.setEmprestado(true);
+   System.out.println("Livro emprestado com sucesso.");
+  } else {
+   System.out.println("Ação: O livro já está emprestado.");
+  }
+ }
+
+ public void devolveLivro(Livro livro) {
+  if (livro.getEmprestado()) {
+   livro.setEmprestado(false);
+   System.out.println("Ação: Livro devolvido com sucesso.");
+  } else {
+   System.out.println("Ação: O livro já está disponível.");
+  }
+ }
 
 }
