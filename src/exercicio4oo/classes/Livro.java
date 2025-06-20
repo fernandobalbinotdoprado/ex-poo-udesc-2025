48a60dd9a55413c8e813feeb5c97a4ee9aaaee10
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-06-19 23:45:31 -0300

    Correção classe Livro e respectivo teste

--- src/exercicio4oo/classes/Livro.java
+++ src/exercicio4oo/classes/Livro.java
@@ -1,5 +1,5 @@
 package exercicio4oo.classes;
-// .
+
 public class Livro {
 
  private String titulo;
@@ -37,5 +37,32 @@ public class Livro {
 
  public void setEmprestado(boolean emprestado) {
   this.emprestado = emprestado;
+ }
+
+ public void verificaEmprestimo(Livro livro) {
+  if (livro.getEmprestado()) {
+   System.out.println("O livro está emprestado.");
+  } else {
+   System.out.println("O livro está disponível.");
+  }
+ }
+
+ public void emprestaLivro(Livro livro) {
+  System.out.println("Estado antes de emprestar: " + livro.getEmprestado());
+  if (!livro.getEmprestado()) {
+   livro.setEmprestado(true);
+   System.out.println("Livro emprestado com sucesso.");
+  } else {
+   System.out.println("Ação: O livro já está emprestado.");
+  }
+ }
+
+ public void devolveLivro(Livro livro) {
+  if (livro.getEmprestado()) {
+   livro.setEmprestado(false);
+   System.out.println("Ação: Livro devolvido com sucesso.");
+  } else {
+   System.out.println("Ação: O livro já está disponível.");
+  }
  }
 }
--- src/exercicio4oo/testes/TestaLivro.java
+++ src/exercicio4oo/testes/TestaLivro.java
@@ -1,6 +1,6 @@
 package exercicio4oo.testes;
 import exercicio4oo.classes.Livro;
-// .
+
 class TestaLivro {
     public static void main(String[] args) {
         Livro livro = new Livro();
@@ -16,41 +16,14 @@ class TestaLivro {
         System.out.println("Emprestado?: " + livro.getEmprestado());
 
         System.out.println("\nVerificando situação...");
-        verificaEmprestimo(livro);
+        livro.verificaEmprestimo(livro);
 
         System.out.println("\nTentando devolver o livro...");
-        devolveLivro(livro);
-        verificaEmprestimo(livro);
+        livro.devolveLivro(livro);
+        livro.verificaEmprestimo(livro);
 
         System.out.println("\nTentando emprestar o livro novamente...");
-        emprestaLivro(livro);
-        verificaEmprestimo(livro);
-    }
-
-    static void verificaEmprestimo(Livro livro) {
-        if (livro.getEmprestado()) {
-            System.out.println("O livro está emprestado.");
-        } else {
-            System.out.println("O livro está disponível.");
-        }
-    }
-
-    static void emprestaLivro(Livro livro) {
-        System.out.println("Estado antes de emprestar: " + livro.getEmprestado());
-        if (!livro.getEmprestado()) {
-            livro.setEmprestado(true);
-            System.out.println("Livro emprestado com sucesso.");
-        } else {
-            System.out.println("Ação: O livro já está emprestado.");
-        }
-    }
-
-    static void devolveLivro(Livro livro) {
-        if (livro.getEmprestado()) {
-            livro.setEmprestado(false);
-            System.out.println("Ação: Livro devolvido com sucesso.");
-        } else {
-            System.out.println("Ação: O livro já está disponível.");
-        }
+        livro.emprestaLivro(livro);
+        livro.verificaEmprestimo(livro);
     }
 }
